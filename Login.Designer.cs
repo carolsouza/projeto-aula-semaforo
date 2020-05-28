445263dc9f6dd13e540e9ac27c9e6558d0a85593
Carol Souza <caah.souza@live.com>
2020-05-27 21:25:30 -0300

    finalizado

---
+++ Login.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Semaforo
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblSenha = new System.Windows.Forms.Label();
+            this.TxtBoxSenha = new System.Windows.Forms.TextBox();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblSenha
+            // 
+            this.LblSenha.AutoSize = true;
+            this.LblSenha.Location = new System.Drawing.Point(45, 35);
+            this.LblSenha.Name = "LblSenha";
+            this.LblSenha.Size = new System.Drawing.Size(41, 13);
+            this.LblSenha.TabIndex = 0;
+            this.LblSenha.Text = "Senha:";
+            this.LblSenha.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TxtBoxSenha
+            // 
+            this.TxtBoxSenha.Location = new System.Drawing.Point(92, 32);
+            this.TxtBoxSenha.Name = "TxtBoxSenha";
+            this.TxtBoxSenha.Size = new System.Drawing.Size(100, 20);
+            this.TxtBoxSenha.TabIndex = 1;
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOk.Location = new System.Drawing.Point(37, 89);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(75, 23);
+            this.BtnOk.TabIndex = 2;
+            this.BtnOk.Text = "Ok";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(153, 89);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(75, 23);
+            this.BtnSair.TabIndex = 3;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 137);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.TxtBoxSenha);
+            this.Controls.Add(this.LblSenha);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblSenha;
+        private System.Windows.Forms.TextBox TxtBoxSenha;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
